a0bdbf7348a400ccb4c23eed35b16f71a771f86e
Adam Prystupa <prystupaadam@gmail.com>
2025-04-03 16:54:14 +0200

    outputs added

--- part1/main.cpp
+++ part1/main.cpp
@@ -8,17 +8,17 @@ int main() {
     std::vector<Task> tasks;
     int currentTime = 0;
     int cmax = 0;
-    loadTasksFromFile("tests/SCHRAGE2.dat", tasks);
+    loadTasksFromFile("../tests/SCHRAGE5.dat", tasks);
 
     // sortByQj(tasks, true);
     // optimizeGroupsByCmax(tasks, false);
     // printTasks(tasks);
-    // findOptimalTaskOrderWithThreads(tasks,8);
+    //findOptimalTaskOrder(tasks);
     //findOptimalTaskOrderWithThreads(tasks, 8);
-    // cmax = calculateCmax(tasks);
-    schrage(tasks);
-    // schrageWithHeap(tasks);
-    // std::cout << "Cmax: " << cmax << "\n";
+    //cmax = calculateCmax(tasks);
+    //schrage(tasks);
+     schrageWithHeap(tasks);
+    std::cout << "Cmax: " << cmax << "\n";
     //printTasks(tasks);
     
 
